c1ebdaa7525ca6679d6dafeab873faade2599507
diomasaki <sendeross99@gmail.com>
2022-09-12 21:50:20 +0700

    commit

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,25 @@
+import "./productList.css";
+import Product from "../product/Product";
+import { products } from "../../data";
+
+const ProductList = () => {
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Create & inspire. It's Us</h1>
+        <p className="pl-desc">
+          We are a professional team that your work has been waiting for.
+          Web development, beautiful art & a whole lot more awaits
+          inside.
+        </p>
+      </div>
+      <div className="pl-list">
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://lh3.googleusercontent.com/desEwkFUL1GQMEq3LZisqN5Mkq-8nFxjY3KJUCVQ3jrnBB3af_HLqKsrITqJh4jMfIwgqXkxqLoPOB2zJSrEJaH-0dLoEqXdTs5OwT0=w600",
+      link: "https://medium.com/@DopeCatNFTs/what-is-an-nft-in-the-art-world-f2d8dce0a914",
+    },
+    {
+      id: 2,
+      img: "https://lh3.googleusercontent.com/QnoUyPtVIDng4gXTXMFun7K2P9ss-QmQ7Wpwb3t3pNIqYxOIzqikm_xHImXg9HfNOxeOS61kwdvyrQc_8-9iPLv6eYzAjWgLuJJViQ=w600",
+      link: "https://medium.com/@DopeCatNFTs/what-is-an-nft-in-the-art-world-f2d8dce0a914",
+    },
+    {
+      id: 3,
+      img: "https://lh3.googleusercontent.com/-nkpgNyjVV2OelajyXYC1GJuXVcsk5KuMiCDHtztqC3AUzDyyhz1lhgjvzOUTC57pqbi9Ao9yck5gcVIKxNCrLNLM7-UipxlSTJYBPQ=w600",
+      link: "https://medium.com/@DopeCatNFTs/what-is-an-nft-in-the-art-world-f2d8dce0a914",
+    },
+    {
+      id: 4,
+      img: "https://lh3.googleusercontent.com/xwxIfX-OqKhfQLjAZDHYchlATDq-fSvgzxDd7PD-bfXYBCCgIPWs0HeOZgDfXzoJr-cpSbcb4AUNirCr8qS7-j12B93jZOYbgClNgg=w600",
+      link: "https://medium.com/@DopeCatNFTs/what-is-an-nft-in-the-art-world-f2d8dce0a914",
+    },
+    {
+      id: 5,
+      img: "https://lh3.googleusercontent.com/bTp-HpLvrrEU_AJYHLdz_iGm6LH9-aqfXD0UD899b84oxw3EiqK6wPfk8eruWkFKFNXnmYjL6Oja2aeLdgeUF80OxfAfhXSjcDF6AQ=w304",
+      link: "https://medium.com/@DopeCatNFTs/what-is-an-nft-in-the-art-world-f2d8dce0a914",
+    },
+    {
+      id: 6,
+      img: "https://lh3.googleusercontent.com/KJL_loMKQYO5MV-1IiB5h6aw-0x0uSRbFkRWeKUaSk5sdTB4IHK50pLAqRC_Z1HxVW0oZFDPKaQxOpCkdF6DVvZskC9xZBKC0QWR=w600",
+      link: "https://medium.com/@DopeCatNFTs/what-is-an-nft-in-the-art-world-f2d8dce0a914",
+    },
+  ];
